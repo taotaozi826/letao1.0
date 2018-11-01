b0e620877974507a317e7fee249c5bed0dc59b2f
taotaozi826 <741473511@qq.com>
2018-11-01 19:30:24 +0800

    一级分类页面渲染,分页,添加功能完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -1,6 +1,7 @@
 $(function () {
     var currentPage = 1;
     var pageSize = 5
+    // 1. 发送ajax请求数据, 进行渲染
     render();
     function render() {
         $.ajax({
@@ -32,6 +33,53 @@ $(function () {
             }
         })
     }
+    // 2. 点击添加按钮, 显示添加模态框
+    $("#addBtn").click(function () {
+      $("#addModal").modal("show");
+    });
     
+    // 3. 表单校验
+    $("#form").bootstrapValidator({
+        // 配置图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',   // 校验成功
+            invalid: 'glyphicon glyphicon-remove',   // 校验失败
+            validating: 'glyphicon glyphicon-refresh'  // 校验中
+        },
     
+        // 配置需要校验的字段
+        fields:{
+            categoryName:{
+                validators:{
+                    notEmpty:{
+                        message:"请输入一级分类"
+                    }
+                }
+            }
+        }
+    });
+    
+    // 4. 注册表单校验成功事件, 阻止默认的提交, 通过 ajax 提交
+    $("#form").on("success.form.bv",function (e) {
+        // console.log(e);
+      e.preventDefault();
+      // console.log($("#form").serialize());
+      
+      $.ajax({
+          type:"post",
+          url:"/category/addTopCategory",
+          data: $("#form").serialize(),
+          dataType:"json",
+          success:function (info) {
+            console.log(info);
+              // 关闭模态框
+            $("#addModal").modal("hide");
+              // 页面重新渲染第1页
+            currentPage = 1;
+            render();
+              // 调用 resetForm 进行重置
+              $("#form").data("bootstrapValidator").resetForm(true);
+          }
+      });
+    })
 })
